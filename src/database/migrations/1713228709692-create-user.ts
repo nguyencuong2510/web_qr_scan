98474e412688dafb7a3428c7557a84ddf1e5f1de
thinh <thinhngan99@gmail.com>
2024-09-24 21:39:28 +0700

    update init password

--- src/database/migrations/1713228709692-create-user.ts
+++ src/database/migrations/1713228709692-create-user.ts
@@ -4,7 +4,7 @@ import * as bcrypt from 'bcrypt';
 export class CreateUser1713228709692 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash('Nh@ttha!tr@', salt);
+    const hash = await bcrypt.hash('Nhatthaitra2023@.@', salt);
 
     await queryRunner.query(`
         INSERT INTO "user" (username, password) VALUES ('admin', '${hash}');
